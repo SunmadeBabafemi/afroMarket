ba4a4e31176c50746b3b6f43a5e75435fdc5b1e1
Olasunmade Babafemi <babafemiolasunmade@gmail.com>
2022-10-24 17:15:01 +0100

    full flow

--- src/db/models/Merchants.js
+++ src/db/models/Merchants.js
@@ -3,7 +3,7 @@ const {Model} = require('sequelize')
 module.exports = (sequelize, DataTypes) =>{
   class Merchant extends Model {
     static associate(models){
-        this.hasMany(models.Product, {as: 'Product'}),
+        this.hasMany(models.Product, {as: 'Product'})
         this.hasMany(models.Inventory, {as: 'Inventory'})
 
     }
@@ -25,6 +25,15 @@ module.exports = (sequelize, DataTypes) =>{
         defaultValue: 'public limited company'
       },
       business_description: {
+        type: DataTypes.STRING,
+      },
+      bank_name: {
+        type: DataTypes.STRING
+      },
+      account_name: {
+        type: DataTypes.STRING
+      },
+      account_number:{
         type: DataTypes.STRING,
       },
       bank_verification_number:{
